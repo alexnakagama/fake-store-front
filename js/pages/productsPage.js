84b0445c274b3ff28d139ee34321c20a2629f8dc
alex <alexnakagama4@gmail.com>
2026-03-28 19:53:07 -0300

    feat(products): render products card dynamically

--- js/pages/productsPage.js
+++ js/pages/productsPage.js
@@ -3,8 +3,22 @@
 import { getProducts } from "../api/products.js";
 
 getProducts().then((products) => {
-    
+
+    let html = "";
+
     products.forEach(p => {
-        let template = ''
-    })
+        let template = `
+        <div class="col">
+            <div class="card">
+                <img src="${p.image}" class="card-img-top" alt="${p.title}">
+                <div class="card-body">
+                    <h5 class="card-title">${p.title}</h5>
+                    <p class="card-text">${p.description}</p>
+                </div>
+            </div>
+        </div>
+        `;
+        html += template;
+    });
+    document.getElementById("products-list").innerHTML = html
 })
